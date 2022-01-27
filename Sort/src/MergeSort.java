51c42c269a6b21d832929a65cba93a456ae9ec02
Moritz van Hettinga <87984786+MOJO196@users.noreply.github.com>
2022-01-27 10:13:23 +0100

    Sort

--- Sort/src/MergeSort.java
+++ Sort/src/MergeSort.java
@@ -1,29 +1,99 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MergeSort {
-    public static void main(String[] args) {
-        int[] newArray = {8, 5, 4, 2, 9, 11, 14, 2};
+public class MergeSort
+{
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-        Sort(newArray);
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
-    static int[] Sort(int[] array) {
-        List<int[]> Layers = new ArrayList();
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
 
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < array.length; i += 2) {
-                Layers.add(new int[]{array[i], array[i + 1]});
-            }
-        } else {
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
         }
+    }
 
+    /* A utility function to print array of size n */
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
-        for (int i = 0; i < array.length; i++) {
+    // Driver code
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
 
-        }
+        System.out.println("Given Array");
+        printArray(arr);
 
-        return array;
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
     }
 }
